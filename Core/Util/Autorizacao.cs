d0e373110c829ffb29ddd12f516faa96eee5626f
DrFaelSan <rafaelvplima@gmail.com>
2019-09-18 17:14:46 -0300

    Tickets Core implementado 18/09

---
+++ Core/Util/Autorizacao.cs
@@ -0,0 +1,26 @@
+﻿using Model;
+using System;
+using System.Linq;
+namespace Core.Util
+{
+    public static class Autorizacao
+    {
+        public static bool ValidarUsuario(string Usertoken, ServiceContext serviceContext)
+        {
+
+            if (!Guid.TryParse(Usertoken, out Guid token) || !serviceContext.Usuarios.Any(e => e.Id == token))
+                return false;
+
+            return true;
+        }
+
+        public static bool GuidValidation(string ID)
+        {
+            if (string.IsNullOrEmpty(ID))
+                return false;
+
+            return Guid.TryParse(ID, out var guid);
+        }
+
+    }
+}
--- Model/Ticket.cs
+++ Model/Ticket.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Model
 {
     public class Ticket : Base
     {
+        [ForeignKey("Usuarios")]
         public Guid ClienteId { get; set; }
-        public List<Resposta> lstRespostas { get; set; } 
-        public Status Status { get; set; } 
+
+        [NotMapped]
+        public List<Resposta> lstRespostas { get; set; }
+
+        public Status? Status { get; set; } 
         public string Titulo { get; set; } 
         public string Tipo { get; set; } 
-        public Avaliacao Avaliacao { get; set; } 
+        public Avaliacao? Avaliacao { get; set; } 
     }
 }
