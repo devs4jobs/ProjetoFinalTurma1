16319649cdc1f33e5f774c6ed29c4464323546b2
matheus <matheus-397@bol.com.br>
2019-09-21 11:34:05 -0300

    savechanges paliativo

--- Model/ServiceContext.cs
+++ Model/ServiceContext.cs
@@ -9,19 +9,28 @@ namespace Model
        public DbSet<Usuario> Usuarios { get; set; }
        public DbSet<Resposta> Respostas { get; set; }
 
+       public override int SaveChanges()
+       {
+           foreach (var Tipo in ChangeTracker.Entries<Usuario>())
+               if (Tipo.State == EntityState.Modified || Tipo.State == EntityState.Added)
+               {
+                   Tipo.Entity.Nome = Tipo.Entity.Nome.ToUpper();
+                   Tipo.Entity.Tipo = Tipo.Entity.Tipo.ToUpper();
+                   Tipo.Entity.Email = Tipo.Entity.Email.ToUpper();
+               }
 
-        public override int SaveChanges()
-        {
-            foreach(var Tipo in ChangeTracker.Entries<Usuario>())
-            {
-                if (Tipo.State == EntityState.Modified || Tipo.State == EntityState.Added)
-                {
-                    Tipo.Entity.Nome = Tipo.Entity.Nome.ToUpper();
-                    Tipo.Entity.Tipo = Tipo.Entity.Tipo.ToUpper();
-                    Tipo.Entity.Email = Tipo.Entity.Email.ToUpper();
-                }
-            }
-            return base.SaveChanges();
-        }
+           foreach (var Tipo2 in ChangeTracker.Entries<Ticket>())
+               if (Tipo2.State == EntityState.Modified || Tipo2.State == EntityState.Added)
+               {
+                   Tipo2.Entity.Titulo = Tipo2.Entity.Titulo.ToUpper();
+                   Tipo2.Entity.Mensagem = Tipo2.Entity.Mensagem.ToUpper();
+               }
+
+           foreach (var Tipo3 in ChangeTracker.Entries<Resposta>())
+               if (Tipo3.State == EntityState.Modified || Tipo3.State == EntityState.Added)
+                   Tipo3.Entity.Mensagem = Tipo3.Entity.Mensagem.ToUpper();
+
+           return base.SaveChanges();
+       }
     }
 }
