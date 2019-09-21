bcb9aff48834e0215bb896a6a19ca2209694b824
DrFaelSan <rafaelvplima@gmail.com>
2019-09-21 15:42:04 -0300

    implementações rafael

---
+++ Model/Views/RespostaView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Model
+{
+    public class RespostaView
+    {
+        public string Mensagem { get; set; }
+        public Guid? TicketId { get; set; }
+    }
+
+    public class RespostaUpdateView
+    {
+        public string Mensagem { get; set; }
+    }
+}
