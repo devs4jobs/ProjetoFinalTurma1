e1a4b34cf5bad56d9b73b0b231c75522f3dd773d
guibolt <guilhermedosreiss@gmail.com>
2019-09-24 17:20:41 -0300

    changes

--- Core/Startup.cs
+++ Core/Startup.cs
@@ -6,7 +6,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Model;
-using Swashbuckle.AspNetCore.Swagger;
 using System.Reflection;
 using System.IO;
 using System;
