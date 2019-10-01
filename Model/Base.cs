ceae029ba58e88f09b2e6ec060f8de334b5a3411
guibolt <guilhermedosreiss@gmail.com>
2019-10-01 17:06:58 -0300

    change

--- Model/Base.cs
+++ Model/Base.cs
@@ -9,8 +9,9 @@ namespace Model
     {
         [Key]
         public Guid Id { get; private set; } = Guid.NewGuid();
-        public DateTime DataCadastro { get; set; } = TimeZoneInfo.ConvertTimeToUtc(DateTime.SpecifyKind(DateTime.UtcNow, DateTimeKind.Unspecified), TimeZoneInfo.GetSystemTimeZones().FirstOrDefault(c => c.DisplayName == "(UTC+03:00) Moscow, St. Petersburg"));
+        public DateTime DataCadastro { get; set; } = DateTime.Now;
 
     }
 }
 
+ 
